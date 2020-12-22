49141b9da829558247cb83db503af24282c9859f
Ignacio Ricart <ignacio@poli.ufrj.br>
2020-12-22 03:13:47 +0100

    add working version using ROS pkg

--- src/behavior_tree/src/hello_BT.cpp
+++ src/behavior_tree/src/hello_BT.cpp
@@ -1,57 +1,61 @@
-#include "behaviortree_cpp_v3/bt_factory.h"
+#include <behaviortree_cpp_v3/bt_factory.h>
 
-//#define MANUAL_STATIC_LINKING
+// file that contains the custom nodes definitions
+// #include "dummy_nodes.h"
 
-#ifdef MANUAL_STATIC_LINKING
-#include "dummy_nodes.h"
-#endif
+// Example of custom SyncActionNode (synchronous action)
+// without ports.
+class ApproachObject : public BT::SyncActionNode
+{
+  public:
+    ApproachObject(const std::string& name) :
+        BT::SyncActionNode(name, {})
+    {
+    }
 
-using namespace BT;
+    // You must override the virtual function tick()
+    BT::NodeStatus tick() override
+    {
+        std::cout << "ApproachObject: " << this->name() << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
 
-/** Behavior Tree are used to create a logic to decide what
- * to "do" and when. For this reason, our main building blocks are
- * Actions and Conditions.
- *
- * In this tutorial, we will learn how to create custom ActionNodes.
- * It is important to remember that NodeTree are just a way to
- * invoke callbacks (called tick() ). These callbacks are implemented by the user.
- */
+// Simple function that return a NodeStatus
+BT::NodeStatus CheckBattery()
+{
+    std::cout << "[ Battery: OK ]" << std::endl;
+    return BT::NodeStatus::SUCCESS;
+}
 
-// clang-format off
-static const char* xml_text = R"(
- <root main_tree_to_execute = "MainTree" >
-     <BehaviorTree ID="MainTree">
-        <Sequence name="root_sequence">
-            <CheckBattery   name="battery_ok"/>
-            <OpenGripper    name="open_gripper"/>
-            <ApproachObject name="approach_object"/>
-            <CloseGripper   name="close_gripper"/>
-        </Sequence>
-     </BehaviorTree>
- </root>
- )";
+// We want to wrap into an ActionNode the methods open() and close()
+class GripperInterface
+{
+public:
+    GripperInterface(): _open(true) {}
 
-// clang-format on
+    BT::NodeStatus open() {
+        _open = true;
+        std::cout << "GripperInterface::open" << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    BT::NodeStatus close() {
+        std::cout << "GripperInterface::close" << std::endl;
+        _open = false;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+private:
+    bool _open; // shared information
+};
 
 int main()
 {
     // We use the BehaviorTreeFactory to register our custom nodes
-    BehaviorTreeFactory factory;
-
-    /* There are two ways to register nodes:
-    *    - statically, i.e. registering all the nodes one by one.
-    *    - dynamically, loading the TreeNodes from a shared library (plugin).
-    * */
-
-#ifdef MANUAL_STATIC_LINKING
-    // Note: the name used to register should be the same used in the XML.
-    // Note that the same operations could be done using DummyNodes::RegisterNodes(factory)
-
-    using namespace DummyNodes;
+    BT::BehaviorTreeFactory factory;
 
     // The recommended way to create a Node is through inheritance.
-    // Even if it requires more boilerplate, it allows you to use more functionalities
-    // like ports (we will discuss this in future tutorials).
     factory.registerNodeType<ApproachObject>("ApproachObject");
 
     // Registering a SimpleActionNode using a function pointer.
@@ -60,19 +64,35 @@ int main()
 
     //You can also create SimpleActionNodes using methods of a class
     GripperInterface gripper;
-    factory.registerSimpleAction("OpenGripper", std::bind(&GripperInterface::open, &gripper));
-    factory.registerSimpleAction("CloseGripper", std::bind(&GripperInterface::close, &gripper));
+    factory.registerSimpleAction("OpenGripper", 
+                                 std::bind(&GripperInterface::open, &gripper));
+    factory.registerSimpleAction("CloseGripper", 
+                                 std::bind(&GripperInterface::close, &gripper));
 
-#else
-    // Load dynamically a plugin and register the TreeNodes it contains
-    // it automated the registering step.
-    factory.registerFromPlugin("./src/behavior_tree/lib/libdummy_nodes_dyn.so");
-#endif
+    // Trees are created at deployment-time (i.e. at run-time, but only 
+    // once at the beginning). 
 
-    // Trees are created at deployment-time (i.e. at run-time, but only once at the beginning).
-    // The currently supported format is XML.
-    // IMPORTANT: when the object "tree" goes out of scope, all the TreeNodes are destroyed
+    // IMPORTANT: when the object "tree" goes out of scope, all the 
+    // TreeNodes are destroyed
+
+    // clang-format off
+    static const char* xml_text = R"(
+    <root main_tree_to_execute = "MainTree" >
+        <BehaviorTree ID="MainTree">
+            <Sequence name="root_sequence">
+                <CheckBattery   name="battery_ok"/>
+                <OpenGripper    name="open_gripper"/>
+                <ApproachObject name="approach_object"/>
+                <CloseGripper   name="close_gripper"/>
+            </Sequence>
+        </BehaviorTree>
+    </root>
+    )";
+
     auto tree = factory.createTreeFromText(xml_text);
+
+    // To have it running from a file
+    // auto tree = factory.createTreeFromFile("./my_tree.xml");
 
     // To "execute" a Tree you need to "tick" it.
     // The tick is propagated to the children based on the logic of the tree.
